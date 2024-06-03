470a6992a0730d7a73f820854b179976c2afd388
Itachi1905 <162597171+Itachi1905@users.noreply.github.com>
2024-06-03 17:09:39 +0530

    Adding Cloner

--- BIT_Anime/express.js
+++ BIT_Anime/express.js
@@ -2,12 +2,19 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
-
+const fs = require("fs")
 // Serve static files from the "public" directory
 app.use(express.static('public'))
-
+app.use((req, res, next) => {
+    console.log(req.headers)
+    req.harry = "Access at";
+    let date = Date.now();
+    fs.appendFileSync("logs.txt", `${Date(date)} is a ${req.method}\n`)
+    console.log(`${Date.now()} is a ${req.method}`)
+    next()
+})
 // Route to serve the main HTML file
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'BIT_Anime.html'))
 })
 app.get('/about',(req, res) => {
